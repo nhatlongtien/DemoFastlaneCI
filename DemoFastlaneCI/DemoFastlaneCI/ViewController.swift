d1e41f45edcc476d75b1f7bd4a36d7ae19c9ae49
Tien Nguyen <tiennl@tinhvan.com>
2022-12-22 13:51:59 +0700

    Create project

---
+++ DemoFastlaneCI/DemoFastlaneCI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoFastlaneCI
+//
+//  Created by Danny Vu on 12/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DemoFastlaneCI/DemoFastlaneCIUITests/DemoFastlaneCIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DemoFastlaneCIUITestsLaunchTests.swift
+//  DemoFastlaneCIUITests
+//
+//  Created by Danny Vu on 12/22/22.
+//
+
+import XCTest
+
+class DemoFastlaneCIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
